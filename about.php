002f5d59e517d98deefac834b6f978d29a983313
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-08-31 23:58:13 +0530

    Adding about page to project

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+    include('header.php');
+?>
+<div class=" container-fluid">
+    <div class="row">
+        <div class="aboutus col-md-6">
+           <center><h1>About Us</h1></center>
+            No more worries about petrol mileage, insurance, and car breakdowns! Company has enabled driving convenience for travellers around the country and is fast expanding its reach to cities including Ahmedabad, Bangalore, Chandigarh, Chennai, Coimbatore, Delhi-NCR, Hyderabad, Jaipur, Kochi, Kolkata, Ludhiana, Mangalore, Mumbai, Mysore, Pune, Siliguri, Vizag, Nagpur, Udaipur, Vijayawada, Surat, Lucknow and Guwahati.
+Self drive cars from Zoomcar have given customers more control, privacy, and freedom. Book a self drive car in any city you visit with the Zoomcar app on your phone and feel at home wherever you go.
+        </div>
+        <div class="col-md-6">
+            
+        </div>
+    </div>
+</div>
