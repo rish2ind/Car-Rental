7046db1f1c523077caaf511804b5c7baf41b957c
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-09-14 18:15:06 +0530

    Adding car booking page....

---
+++ car_booking.php
@@ -0,0 +1,78 @@
+<?php
+    include('header.php');
+?>
+
+    <div class="container-fluid">
+        <div class="row car-details">
+            <div class="col-md-2 car-image">
+                <center><img src="Images/honda.jpg" style="height: 120px;" alt="" class="img-responsive"></center>
+            </div>
+            <div class="col-md-8 car-image">
+                
+            </div>
+            <div class="col-md-2 car-image price">
+                <i class="fas fa-rupee-sign"></i>
+                6,500
+                <p>
+                    <center><a href="#"><button class="btn btn-success">BOOK NOW</button></a></center>
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
+        <div class="row car-details">
+            <div class="col-md-2 car-image">
+                <center><img src="Images/hyundai.jpg" style="height: 120px;" alt="" class="img-responsive"></center>
+            </div>
+            <div class="col-md-8 car-image">
+                
+            </div>
+            <div class="col-md-2 car-image price">
+                <i class="fas fa-rupee-sign"></i>
+                4,499
+                <p>
+                    <center><a href="#"><button class="btn btn-success">BOOK NOW</button></a></center>
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
+        <div class="row car-details">
+            <div class="col-md-2 car-image">
+                <center><img src="Images/mahindra1.jpg" style="height: 120px;" alt="" class="img-responsive"></center>
+            </div>
+            <div class="col-md-8 car-image">
+                
+            </div>
+            <div class="col-md-2 car-image price">
+                <i class="fas fa-rupee-sign"></i>
+                7,599
+                <p>
+                    <center><a href="#"><button class="btn btn-success">BOOK NOW</button></a></center>
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
+        <div class="row car-details">
+            <div class="col-md-2 car-image">
+                <center><img src="Images/tata.jpg" style="height: 120px;" alt="" class="img-responsive"></center>
+            </div>
+            <div class="col-md-8 car-image">
+                
+            </div>
+            <div class="col-md-2 car-image price">
+                <i class="fas fa-rupee-sign"></i>
+                8,499
+                <p>
+                    <center><a href="#"><button class="btn btn-success">BOOK NOW</button></a></center>
+                </p>
+            </div>
+        </div>
+    </div>
+<?php
+    include('footer.php');
+?>
