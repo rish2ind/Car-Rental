f92e93c856e0af0f171c875af392a149c844f405
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-09-21 23:44:55 +0530

    Adding new files

---
+++ checkout_hyundai.php
@@ -0,0 +1,39 @@
+<?php
+    include('header.php');
+    if(isset($_SESSION['user'])){
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 checkout">
+            <h3>Checkout</h3>
+            <hr>
+            <span style="font-size: 18px; font-family: 'Arial';">Booking Fee</span><br>Booking fee excludes fuel cost
+            <span style="float: right; font-size: 18px; color: #3754ec;"><i class="fas fa-rupee-sign"></i>&nbsp;6,500</span>
+            <hr>
+            <div style="background-color: #c6ffc6; padding: 15px; font-size: 18px;">Total Amount : <span style="float: right;"><i class="fas fa-rupee-sign"></i>&nbsp;6,500</span></div>
+        </div>
+        <div class="col-md-4 checkout" style="margin-left: 10px;">
+           <center> <img src="Images/mahindra1.jpg" alt="" class="img-responsive"></center>
+            <center><h2>HYUNDAI CRETA</h2></center>
+             <center>   <h4>(5 SEATER)</h4></center>
+            
+        </div>
+        <a href="final_booking_hyundai.php"><center><button class="btn btn-success" style="position: relative; top: -100px;">CHECKOUT</button></center></a>
+    </div>
+</div>
+
+
+
+
+<?php
+
+include('footer.php');
+    }
+else{
+    echo "<script> 
+            alert('Please login First');
+            window.location.href = 'UserDetails.php';
+        </script>";
+}
+?>
---
+++ final_booking_hyundai.php
@@ -0,0 +1,44 @@
+<?php
+    include('header.php');
+if(isset($_SESSION['user'])){
+?>
+<div class="container">
+    <div class="row checkout">
+        
+            <div style="background-color: #c6ffc6; padding: 15px; font-size: 18px;">Total Amount : <span style="float: right;"><i class="fas fa-rupee-sign"></i>&nbsp;6,500</span>
+        </div></div>
+    <div class="row checkout" style="position: relative; top: -60px;">
+        <div class="col-md-4" style="background-color: #39406d; border-radius: 10px;">
+        <h3 style="color: white;">Payment Methods</h3><hr>
+            <div class="payment">
+                <center><form action="" method="post">
+                   <h4 style="color: white;">Card Payment</h4>
+                    <div class="form-group">                         
+                        <input type="text" name="card_number" class="form-control" placeholder="Enter Card Number" style="width: 300px; padding: 18px; margin-bottom: 5px;" required="">
+                    
+                        <input type="text" name="CVV" class="form-control" placeholder="CVV" style="width: 300px; padding: 18px; margin-bottom: 5px;" required="">
+                        <input type="text" name="expire" class="form-control" placeholder="mm/yyyy" style="width: 300px; padding: 18px; margin-bottom: 5px;" required="">
+                    </div>
+                </form></center>    
+            </div>
+    </div>    
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <center><img src="Images/mahindra1.jpg" alt="" class="img-responsive"></center>
+                <center><h3>HYUNDAI CRETA</h3></center>
+                <center><h4>( 5 SEATER)</h4></center>
+        </div>
+    </div>
+    <a href="#"><center><button class="btn btn-success" style="position: relative; top: -100px;">BOOK NOW</button></center></a>
+</div>
+<?php
+    include('footer.php');
+}
+else{
+    echo "<script> 
+            alert('Please login First');
+            window.location.href = 'UserDetails.php';
+        </script>";
+}
+?>
+
