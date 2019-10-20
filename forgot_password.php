4d02315e61723d015998dddb1f090a1cd376c30a
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-10-20 10:23:49 +0530

    Forgot password page is under construction

---
+++ forgot_password.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forgot Password</title>
+</head>
+<body>
+    <center><h1>This page is under construction</h1></center>
+</body>
+</html>
