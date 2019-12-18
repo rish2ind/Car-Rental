ab5554003bef7d046a6054fe69835908a53dab9e
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-12-18 20:41:41 +0530

    This page is now dynamic

--- checkout.php
+++ checkout.php
@@ -2,25 +2,28 @@
     include('header.php');
     include('connect.php');
     if(isset($_SESSION['user'])){
+        $id = $_REQUEST['id'];
+        $sqlcheckout = "select * from car_details where id = '$id'";
+        $runcheckout = mysqli_query($conn, $sqlcheckout);
 ?>
 
 <div class="container">
-    <div class="row">
+    <div class="row"><?php $datacheckout = mysqli_fetch_array($runcheckout); ?>
         <div class="col-md-7 checkout">
             <h3>Checkout</h3>
             <hr>
             <span style="font-size: 18px; font-family: 'Arial';">Booking Fee</span><br>Booking fee excludes fuel cost
-            <span style="float: right; font-size: 18px; color: #3754ec;"><i class="fas fa-rupee-sign"></i>&nbsp;1,000</span>
+            <span style="float: right; font-size: 18px; color: #3754ec;"><i class="fas fa-rupee-sign"></i>&nbsp;<?php echo $datacheckout['price']; ?></span>
             <hr>
-            <div style="background-color: #c6ffc6; padding: 15px; font-size: 18px;">Total Amount : <span style="float: right;"><i class="fas fa-rupee-sign"></i>&nbsp;1,000</span></div>
+            <div style="background-color: #c6ffc6; padding: 15px; font-size: 18px;">Total Amount : <span style="float: right;"><i class="fas fa-rupee-sign"></i>&nbsp;<?php echo $datacheckout['price']; ?></span></div>
         </div>
         <div class="col-md-4 checkout" style="margin-left: 10px;">
-           <center> <img src="Images/honda.jpg" alt="" class="img-responsive"></center>
-            <center><h2>HONDA CITY</h2></center>
-             <center>   <h4>(5 SEATER)</h4></center>
+           <center> <img src="Admin/Gallery/<?php echo $datacheckout['pic']; ?>" alt="" class="img-responsive"></center>
+            <center><h2><?php echo $datacheckout['name']; ?></h2></center>
+             <center>   <h4>(<?php echo $datacheckout['seat']; ?> SEATER)</h4></center>
             
         </div>
-        <a href="final_booking.php"><center><button class="btn btn-success" style="position: relative; top: -100px;">CHECKOUT</button></center></a>
+        <a href="final_booking.php?id=<?php echo $datacheckout['id']; ?>"><center><button class="btn btn-success" style="position: relative; top: -100px;">CHECKOUT</button></center></a>
     </div>
 </div>
 
